bc61ccfad593b5a986f699b13bb8a87a4c613ab0
Tsukanova Evgeniya <tata@gmail.com>
2023-04-15 19:24:37 +0300

    dz4 china виправлення2

--- src/Dz4/China.java
+++ src/Dz4/China.java
@@ -8,8 +8,10 @@ public class China {
         int minVoin=9;
         int minLuch=35;
         int miVersh=12;
-        double allli = (liVoin+liLuch+liVersh)*860;
-        double allmi = ((minVoin+minLuch+miVersh)*860)*1.5;
+        int sumPipleLi=860;
+        double sumPipleMi=860*1.5;
+        double allli = (liVoin+liLuch+liVersh)*sumPipleLi;
+        double allmi = (minVoin+minLuch+miVersh)*sumPipleMi;
         System.out.println("Загальна сила армій: "+ (allli+allmi));
     }
 }
