9a4390759cba69449c973bd920f29ce8fa8b98ae
Tsukanova Evgeniya <tata@gmail.com>
2023-06-11 18:54:24 +0300

    dz4 china виправлення2

--- src/Dz4/China.java
+++ src/Dz4/China.java
@@ -2,16 +2,36 @@ package Dz4;
 
 public class China {
     public static void main(String[]args){
-        int liVoin=13;
-        int liLuch=24;
-        int liVersh=46;
-        int minVoin=9;
-        int minLuch=35;
-        int miVersh=12;
-        int sumPipleLi=860;
-        double sumPipleMi=860*1.5;
-        double allli = (liVoin+liLuch+liVersh)*sumPipleLi;
-        double allmi = (minVoin+minLuch+miVersh)*sumPipleMi;
-        System.out.println("Загальна сила армій: "+ (allli+allmi));
+//        За владу в стародавньому Китаї боролися дві династії: Лі та Мінь.
+//                З кожного боку боролися 3 типи воїнів з такими
+//                показниками атаки:
+//        Лі:
+//        воїн - 13
+//        лучник - 24
+//        вершник - 46
+//
+//        Мінь:
+//        воїн - 9
+//        лучник - 35
+//        вершник - 12
+//        Розрахувати: загальний показник атаки для обох династій,
+//        якщо відомо, що воїнів династії Лі кожного типу було 860,
+//        а кількість воїнів династії Мінь кожного типу
+//        в півтора рази більша.
+//        Вивести в консоль значення загальної атаки кожної армії;
+        int powerLi=860;
+        double powerMin=860*1.5;
+
+        int liWarrior=13;
+        int liArcher=24;
+        int liRider=46;
+        double sumLi=(liArcher+liRider+liWarrior)*powerLi;
+        System.out.println("Лі загальна атака:"+sumLi);
+
+        int minWarrior=13;
+        int minArcher=13;
+        int minRider=13;
+        double sumMin=(minRider+minArcher+minWarrior)*powerMin;
+        System.out.println("Miнь загальна атака: "+sumMin);
     }
 }
